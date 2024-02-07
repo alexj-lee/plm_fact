9eeee8e6be413912195bd9c45f5955600af15582
alex lee <alex.lee@ucsf.edu>
2024-02-07 11:33:46 -0800

    remove fixture for single test

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,7 +3,7 @@ import plm_fact as plm
 import numpy as np
 
 
-@pytest.fixture
+# @pytest.fixture
 def test_load_fn_rosetta():
     # should split this up
     train_loader, test_loader = plm.data.get_data_loaders_rosetta()
@@ -22,4 +22,4 @@ def test_load_fn_rosetta():
     batch = np.vstack((first, second))
     assert batch.shape == (2, 1280), 'Batch dimensions are incorrect'
 
-    return batch  # maybe use this later
+    # return batch  # maybe use this later in fixture
